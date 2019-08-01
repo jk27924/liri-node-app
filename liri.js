6231738930d9578e58494394214143d2be1264e5
Jihong Kim <jk279242naver.com>
2019-08-01 02:17:43 -0500

    declared var action for argv[2], and put four cases under switch. PLus, declared var sliceJoin for the inputs for four diffrerent cases so I do not have duplicate code on four cases and functions.

--- liri.js
+++ liri.js
@@ -1,6 +1,45 @@
 require("dotenv").config();
 
-
 var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+var moment = require("moment");
+var axios = require("axios");
+
+var fs = require('fs');
+var OmdbApi = require('omdb-api-pt')
+var bandsintown = require('bandsintown')("106ad69002bde46c02cdebb861ad5e84");
+
+var action = process.argv [2];
+function switchAction() {
+    switch (action) {
+        case "concert-this":
+        getConcert();
+        break;
+
+        case "spotify-this-song":
+        getSong();
+        break;
+
+        case "movie-this":
+        getMovie();
+        break;
+
+        case "do-what-it-says":
+        doWhatItSays();
+        break;
+    }
+}
+
+var sliceJoin = process.argv.slice(3).join(" ");
+
+
+
+// if (action === "concert-this") {
+//     var band = process.argv.slice(3).join(" ");
+//     axios.get ("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(function(result) {
+//         console.log(result); 
+//     })
+// } 
