820d772197e5015e2a3318ba4fb82b260ea4de1c
Jihong Kim <jk279242naver.com>
2019-08-01 21:55:05 -0500

    completed the function for Bands In Town. Put artist name, venue name, venue location, and datetime for the venue. Plus I used moment.js to change the format of Datetime as the instruction required.

--- liri.js
+++ liri.js
@@ -13,7 +13,9 @@ var OmdbApi = require('omdb-api-pt')
 var bandsintown = require('bandsintown')("106ad69002bde46c02cdebb861ad5e84");
 
 var action = process.argv [2];
-function switchAction() {
+var actionTwo = process.argv.slice(3).join(" ");
+
+// function switchAction() {
     switch (action) {
         case "concert-this":
         getConcert();
@@ -31,15 +33,43 @@ function switchAction() {
         doWhatItSays();
         break;
     }
+// }
+
+
+// #1 Bands in Town
+function getConcert () {
+    var artist = actionTwo;
+    var bandsQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+
+
+    axios.get(bandsQueryUrl).then(function(response) {
+        console.log (
+            "Artist: " + artist + "\nVenue: " + response.data[0].venue.name + "\nLocation: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country + "\nDate: " + moment(response.data[0].datetime).format("MM/DD/YYYY")
+        );
+    })
+
+// * Name of the venue
+// * Venue location
+// * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
 }
 
-var sliceJoin = process.argv.slice(3).join(" ");
 
 
 
-// if (action === "concert-this") {
-//     var band = process.argv.slice(3).join(" ");
-//     axios.get ("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(function(result) {
-//         console.log(result); 
-//     })
-// } 
+
+
+// #2 Spotify
+
+
+// #3 OMDB
+
+
+// #4 Do What It Says
+
+
+
+
+
+
