09341e8542c88652946a2ef2d0a205d1c2e6c9af
Jihong Kim <jk279242naver.com>
2019-08-02 01:08:21 -0500

    added an IF statement for spotify in case of no song is provided. It is supposed to load I Saw the Sign by Ace of Base. It is loading I Saw the Sign, but it is not by Ace of Base, so still needs to work on this.

--- liri.js
+++ liri.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var axios = require("axios");
+
+var bandsintown = require('bandsintown')("106ad69002bde46c02cdebb861ad5e84");
+var moment = require("moment");
+
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-var moment = require("moment");
-var axios = require("axios");
+var OmdbApi = require('omdb-api-pt')
 
 var fs = require('fs');
-var OmdbApi = require('omdb-api-pt')
-var bandsintown = require('bandsintown')("106ad69002bde46c02cdebb861ad5e84");
 
 var action = process.argv [2];
 var actionTwo = process.argv.slice(3).join(" ");
@@ -43,18 +45,29 @@ function getConcert () {
 
     axios.get(bandsQueryUrl).then(function(response) {
         console.log (
-            "Artist: " + artist + "\nVenue: " + response.data[0].venue.name + "\nLocation: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country + "\nDate: " + moment(response.data[0].datetime).format("MM/DD/YYYY")
+            "----------------------------------------------" + 
+            "Artist: " + artist + 
+            "\nVenue: " + response.data[0].venue.name + 
+            "\nLocation: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country + 
+            "\nDate: " + moment(response.data[0].datetime).format("MM/DD/YYYY") +
+            "\n----------------------------------------------"
         );
     })
 }
+
 
 // #2 Spotify
 function getSong () {
     var song = actionTwo;
 
+    if (song = " ") {
+        song = "I Saw the Sign";
+        // How to make this by Ace of Base?
+    }
+
     spotify.search({ type: 'track', query: song }).then(function(response) {
         console.log (
-            "\n----------------------------------------------" + 
+            "----------------------------------------------" + 
             "\nArtist(s): " + response.tracks.items[0].album.artists[0].name +
             "\nSong: " + response.tracks.items[0].name +
             "\nSpotify Link: " + response.tracks.items[0].external_urls.spotify +
@@ -64,6 +77,7 @@ function getSong () {
     });
 }
 
+
 // #3 OMDB
 function getMovie () {
     var movie = actionTwo;
@@ -71,17 +85,27 @@ function getMovie () {
 
     axios.get(omdbQueryUrl).then(function(response) {
         console.log(
-            "Title: " + movie + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nCountry Produced: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors
+            "----------------------------------------------" + 
+            "Title: " + movie + 
+            "\nYear: " + response.data.Year + 
+            "\nIMDB Rating: " + response.data.imdbRating + 
+            "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + 
+            "\nCountry Produced: " + response.data.Country + 
+            "\nLanguage: " + response.data.Language + 
+            "\nPlot: " + response.data.Plot + 
+            "\nActors: " + response.data.Actors +
+            "\n----------------------------------------------"
         );
     });
 }
+
 
 // #4 Do What It Says
 function doWhatItSays () {
     fs.readFile("random.txt", "utf-8", function(error,data) {
 
         if (error) {
-            return console.log(red(error));
+            return console.log(error);
         } else {
             var output = data.split(", ");
             getSong(txt[1]);
