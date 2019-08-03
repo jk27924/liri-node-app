684939a35d7b69c0225fab3d8250104a5ffc7eb1
Jihong Kim <jk279242naver.com>
2019-08-03 18:42:44 -0500

    added comments for liri.js.

--- liri.js
+++ liri.js
@@ -1,26 +1,23 @@
 require("dotenv").config();
 
+// Variables
+
+// saving required files/npm packages as variables
 var keys = require("./keys.js");
-
 var axios = require("axios");
-
 var bandsintown = require("bandsintown")("106ad69002bde46c02cdebb861ad5e84");
 var moment = require("moment");
-
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-
 var OmdbApi = require("omdb-api-pt")
-
 var fs = require("fs");
 
-
-
+// saving user input command
 var action = process.argv [2];
 var actionTwo = process.argv.slice(3).join(" ");
 
 
-
+// Switch cases to run the appropriate function based on the user's command
 function switchAction() {
     switch (action) {
         case "concert-this":
@@ -48,6 +45,7 @@ function getConcert () {
     var artist = actionTwo;
     var bandsQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
+    // Axios + function(response)
     axios.get(bandsQueryUrl).then(function(response) {
         console.log (
             "\n----------------------------------------------" + 
@@ -58,8 +56,8 @@ function getConcert () {
             "\n----------------------------------------------"
         );
 
+        // Variables for log.txt
         var currentMoment = moment().format("MMMM Do YYYY, h:mm:ss a");
-
         var logText =
         "\n--------------------------------------------------------" + 
         "\nYOUR REQUESTED | CONCERT | INFORMATION! ENJOY ^_^" +
@@ -70,6 +68,7 @@ function getConcert () {
         "\nDate: " + moment(response.data[0].datetime).format("MM/DD/YYYY") +
         "\n--------------------------------------------------------";
 
+        // Appending the Variables to log.txt file to keep the search record
         fs.appendFile("log.txt", logText, function (){});
     })
 }
@@ -79,13 +78,13 @@ function getConcert () {
 function getSong () {
     var song = actionTwo;
 
+    // If No song is provided, set "The Sign" by Ace of Base for Default
     if (!song) {
         song = "The Sign Ace of Base";
     }
 
+    // function(response)
     spotify.search({ type: 'track', query: song }).then(function(response) {
-        
-        
         console.log (
             "\n----------------------------------------------" + 
             "\nArtist(s): " + response.tracks.items[0].album.artists[0].name +
@@ -95,17 +94,19 @@ function getSong () {
             "\n----------------------------------------------"
         );
 
+        // Variables for log.txt
         var currentMoment = moment().format("MMMM Do YYYY, h:mm:ss a");
-
         var logText =
         "\n--------------------------------------------------------" + 
         "\nYOUR REQUESTED | SONG | INFORMATION! ENJOY ^_^" + 
-        "\n<<-- Search Request on: " + currentMoment + " -->>" +               "\nArtist(s): " + response.tracks.items[0].album.artists[0].name +
+        "\n<<-- Search Request on: " + currentMoment + " -->>" +
+        "\nArtist(s): " + response.tracks.items[0].album.artists[0].name +
         "\nSong: " + response.tracks.items[0].name +
         "\nSpotify Link: " + response.tracks.items[0].external_urls.spotify +
         "\nAlbum: " + response.tracks.items[0].album.name +
         "\n--------------------------------------------------------"
 
+        // Appending the Variables to log.txt file to keep the search record
         fs.appendFile("log.txt", logText, function (){});
     });
 }
@@ -115,13 +116,14 @@ function getSong () {
 function getMovie () {
     var movie = actionTwo;
 
+    // if No movie is provided, set "Mr Nobody" for Default
     if (!movie) {
         movie = "Mr Nobody";
     }
 
     var omdbQueryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
 
-
+    // Axios + function(response)
     axios.get(omdbQueryUrl).then(function(response) {
         console.log(
             "\n----------------------------------------------" + 
@@ -136,8 +138,8 @@ function getMovie () {
             "\n----------------------------------------------"
         );
 
+        // Variables for log.txt
         var currentMoment = moment().format("MMMM Do YYYY, h:mm:ss a");
-
         var logText =
         "\n--------------------------------------------------------" + 
         "\nYOUR REQUESTED | MOVIE | INFO! ENJOY ^_^" +            
@@ -152,6 +154,7 @@ function getMovie () {
         "\nActors: " + response.data.Actors + 
         "\n--------------------------------------------------------"
 
+        // Appending the Variables to log.txt file to keep the search record
         fs.appendFile("log.txt", logText, function (){});
     });
 }
@@ -159,22 +162,18 @@ function getMovie () {
 
 // #4 do-what-it-says _ random.txt
 function doWhatItSays () {
+    
+    //Read random.txt file
     fs.readFile("random.txt", "utf-8", function(error,data) {
 
         if (error) {
             return console.log(error);
         }
 
-        console.log("data is: " + data);
-
         var output = data.split(",");
-
-        console.log ("Output is: " + output);
 
         action = output[0];
         actionTwo = output[1];
-        console.log("Action is: " + action);
-        console.log("Action Two is: " + actionTwo);
 
         switchAction();
     });
